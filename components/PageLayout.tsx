bab089dd9224203b2f5f95d8f45238316fe89aa1
Fernando Chicaiza <washopilot@yahoo.com>
2022-11-19 09:14:48 -0500

    testing getServerSideProps fetching index.json

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import React from 'react';
 
 type PageLayoutProps = { children?: React.ReactNode; title?: string };
@@ -11,11 +12,20 @@ const PageLayout = ({ children, title = 'NewsApp' }: PageLayoutProps) => {
                 <meta name="description" content="newsapp - the best app to read news " />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <header>🗞 newsapi</header>
+            <header>
+                <div>
+                    <Link href={'/'}>🗞 newsapi</Link>
+                </div>
+                <div>
+                    <Link href={'/about'}>About</Link>
+                </div>
+            </header>
             <main>{children}</main>
             <style jsx>
                 {`
                     header {
+                        display: flex;
+                        justify-content: space-between;
                         padding: 20px;
                     }
                 `}
